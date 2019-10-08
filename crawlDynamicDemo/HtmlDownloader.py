bdf0884cd1c116cd742d629e4fef19eb5e5be082
qiuqiu <guodq12@163.com>
2019-10-08 22:23:15 +0800

    动态获取MTime评分信息，并使用sqlite存储

---
+++ crawlDynamicDemo/HtmlDownloader.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     HtmlDownloader
+   Description :
+   Author :       qiuqiu
+   date：          2019/10/8
+-------------------------------------------------
+"""
+import requests
+
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            return None
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        headers = {'User-Agent': user_agent}
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            r.encoding = 'utf-8'
+            return r.text
+        return None
---
+++ crawlDynamicDemo/ReadMe.md
@@ -0,0 +1,9 @@
+## 动态查询数据
+
+爬取[MTime电影网站](http://www.mtime.com)
+
+使用ajax获取动态数据
+
+使用sqlite作为数据存储
+
+[sqlite图形化工具](https://sqlitebrowser.org/dl/)
