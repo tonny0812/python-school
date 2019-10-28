ec044486516258349a3ef944fd1b8be2e553ba45
guodongqing <guodq12@163.com>
2019-10-28 18:16:25 +0800

    mega api

---
+++ megaapi/mega.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     mega
+   Description :
+                    https://github.com/richardARPANET/mega.py
+   Author :       guodongqing
+   date：          2019/10/28
+-------------------------------------------------
+"""
---
+++ megaapi/readme.md
@@ -0,0 +1,3 @@
+[Api 文档](https://github.com/richardARPANET/mega.py)
+
+[载入包报错解决方法](https://stackoverflow.com/questions/44951456/pip-error-microsoft-visual-c-14-0-is-required)
